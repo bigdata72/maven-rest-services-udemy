0bf8e9bad96398123d3b420586d451083557a794
bigdata72 <reviewking72@gmail.com>
2020-07-04 16:08:12 -0400

    initial commit

---
+++ src/main/java/com/arijit/restservices/udemyrestservices/UdemyRestServicesApplication.java
@@ -0,0 +1,13 @@
+package com.arijit.restservices.udemyrestservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyRestServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyRestServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arijit/restservices/udemyrestservices/UdemyRestServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arijit.restservices.udemyrestservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemyRestServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
